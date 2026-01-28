4a77743a5595ed4acfadc724514e8657f67603e9
osmanjeylani <osmanjeylani00@gmail.com>
2026-01-28 09:39:44 +0100

    updated BookingForm, now able to save booking to api

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,12 +1,63 @@
-import React from 'react'
+import { useState } from 'react'
 
 export const BookingForm = () => {
+
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const booking = {
+      name,
+      phone
+    };
+
+    await  fetch("http://localhost:3001/bookings", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(booking)
+    });
+
+    setName("");
+    setPhone("");
+  }
+
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Namn</label>
+      <input
+      id="name"
+      name="name"
+      type="text"
+      required
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+      />
+
+      <label htmlFor="phone">Telefonnummer</label>
+      <input
+      id="phone"
+      name="phone"
+      type="tel"
+      required
+      value={phone}
+      onChange={(e) => setPhone(e.target.value)}
+      />
+
+      <button type="submit">Bekräfta bokning</button>
+    </form>
+
+  );
+};
+
+
+{/* <form>
         <label htmlFor="Name">Namn: </label>
         <input type="text" name='Name' id='Name' />
         <label htmlFor="Phone">Telefonnummer: </label>
         <input type="text" name="Phone" id="Phone" />
-    </form>
-  )
-}
+    </form> */}
